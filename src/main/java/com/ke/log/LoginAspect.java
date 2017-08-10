172b7b3cf23c0246c3efecaec34934198e3e274c
keisunique <769100646@qq.com>
2017-08-10 18:03:40 +0800

    登陆日志

--- src/main/java/com/ke/log/LoginAspect.java
+++ src/main/java/com/ke/log/LoginAspect.java
@@ -1,10 +1,16 @@
 package com.ke.log;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/8/10.
@@ -25,10 +31,20 @@ public class LoginAspect {
 
     @Before("controllerLog()")
     public void dobefore(JoinPoint joinPoint){
+        HttpServletRequest request = ((ServletRequestAttributes)
+                RequestContextHolder.getRequestAttributes()).getRequest();
+
+
         //获得方法名
         System.out.println("joinPoint.getSignature().getName()="+joinPoint.getSignature().getName());
         //获得类全限定名
         System.out.println("joinPoint.getTarget().getClass().getName()="+joinPoint.getTarget().getClass().getName());
+
+        System.out.println("用户名"+SecurityUtils.getSubject().getPrincipal());
+
+        System.out.println("密码"+request.getParameter("password"));
+
+        //TODO 用户登陆日志，只是输出到了控制台，还没有入库或者写入文件
         System.out.println("前置通知");
 
     }
